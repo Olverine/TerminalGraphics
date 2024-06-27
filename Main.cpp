c40cca5c90c2f51f0a8dea7ed7afd20cd06f51df
Oliver Norin <oliver.david.norin@gmail.com>
2024-06-27 20:04:50 +0100

    Fixed indentation

--- Main.cpp
+++ Main.cpp
@@ -4,34 +4,33 @@
 #include "glm/gtx/euler_angles.hpp"
 #include "Graphics.h"
 
-int main(int argc, char* argv[])
-{
-	vec4* shape = loadShape(argv[1]);
-	initViewPort();
+int main(int argc, char* argv[]) {
+    vec4* shape = loadShape(argv[1]);
+    initViewPort();
 
-	mat4 model = mat4(1.0);
-	mat4 view = getView(vec3(0, 0, 1), vec3(0, 0, 0), vec3(0, 1, 0)); 
-	mat4 projection = getPerspective(radians(90.0f), 0.1, 500);
+    mat4 model = mat4(1.0);
+    mat4 view = getView(vec3(0, 0, 1), vec3(0, 0, 0), vec3(0, 1, 0)); 
+    mat4 projection = getPerspective(radians(90.0f), 0.1, 500);
 
-	float rotX = 0;
-	float rotY = 0;
+    float rotX = 0;
+    float rotY = 0;
 
-	system("clear");
+    system("clear");
 
-	while (true) {
-		clearViewPort();		
+    while (true) {
+        clearViewPort();		
 
-		// Rotate the object
-		rotX += 0.0001f;
-		rotY += 0.0002f;
+        // Rotate the object
+        rotX += 0.0001f;
+        rotY += 0.0002f;
 
-		model = eulerAngleYXZ(rotY, rotX, 0.0f);
+        model = eulerAngleYXZ(rotY, rotX, 0.0f);
 
-		mat4 mp =  projection * view * model;
-		DrawShape(shape, mp);
+        mat4 mp =  projection * view * model;
+        DrawShape(shape, mp);
 
-		cout << out.c_str() << "\033[1;1H";
-	}
+        cout << out.c_str() << "\033[1;1H";
+    }
 
     return 0;
 }
